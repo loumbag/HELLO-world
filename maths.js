af6809899ad617251fc349b6e7a79d6ea72372a7
loumbag <loumbag83@gmail.com>
2016-08-08 15:21:12 +0000

    Add files via upload

---
+++ maths.js
@@ -0,0 +1,24 @@
+function add(x,y)
+{
+	return x+y;
+}
+
+function sub(x,y)
+{
+	return x-y;
+}
+
+function mul(x,y)
+{
+	return x*y;
+}
+
+function div(x,y)
+{
+	return x/y;
+}
+
+function mod(x,y)
+{
+	return x%y;
+}
